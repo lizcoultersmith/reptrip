589981f44cc0e33769dc87beb6ba41ac7feb228f
lizcoultersmith <ecoultersmith@gmail.com>
2023-04-14 12:51:25 +0100

    1251-14423

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='reptrip',
+    version='2.5.0',
+    description='CLIP + FFT/DWT/RGB text-to-image tools',
+    url='https://github.com/lizcoultersmith/reptrip',
+    author='liz coulter-smith',
+    packages=['reptrip'],
+    # packages=find_packages(),
+    install_requires=[],
+    classifiers=[],
+)
